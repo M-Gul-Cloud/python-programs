2e2f075af91d1d1d1303d08767f06acb3592abfe
M.Gul Hamza <techmgulhamza@gmail.com>
2025-09-09 20:26:03 +0500

    Data types and basic operators

---
+++ Data type.py
@@ -0,0 +1,14 @@
+
+# In this file (I am writing Data types of python)
+
+age = 25
+print(age , type(age))
+
+name = "Muhammad Gul Hamza"
+print(name,  type(name))
+
+Height = 6.2
+print(Height, type(Height))
+
+Is_Cloud_Ready = True
+print(Is_Cloud_Ready, type(Is_Cloud_Ready))
